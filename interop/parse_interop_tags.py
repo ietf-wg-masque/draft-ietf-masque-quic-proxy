7b0061159a61f800ca3aaa0f2c65960c0330894d
Christopher Wood <caw@heapingbits.net>
2020-11-10 05:57:57 -0800

    Update interop parser.

--- interop/parse_interop_tags.py
+++ interop/parse_interop_tags.py
@@ -3,24 +3,27 @@ import os
 import sys
 import json
 
-def options_to_tests(fname, output_path, options):
-    tests = []
-    for option in options:
-        tests.append({
-            "name": option["req"],
-            "id": option["req-id"],
-            "kind": option["req-type"],
-        })
+def is_valid_option(option):
+    return ("req" in option and "req-id" in option and "req-type" in option)
+
+def create_test_from_option(option):
+    assert(is_valid_option(option))
+    return {
+        "name": option["req"],
+        "id": option["req-id"],
+        "kind": option["req-type"],
+    }
+
+def create_test_summary_from_options(fname, options):
+    valid_options = filter(lambda option : is_valid_option(option), options)
+    tests = [create_test_from_option(option) for option in valid_options]
     data = {
         "name": draft_title(fname),
         "id": draft_to_test_name(fname),
         "description": "Interoperability test results for " + draft_title(fname),
-        "tests": tests
+        "tests": tests,
     }
-
-    with open(os.path.join(output_path, draft_to_test_name(fname)), "w") as fh:
-        fh.write("export default\n")
-        fh.write(json.dumps(data, indent=4))
+    return data
 
 def draft_to_test_name(fname):
     return os.path.basename(fname).split(".")[0] + ".mjs"
@@ -29,19 +32,28 @@ def draft_title(fname):
     with open(fname, "r") as fh:
         return re.search(r'title: (.*?)\n', fh.read()).group(1).strip()
 
-draft_name = sys.argv[1]
-output_path = sys.argv[2]
-with open(draft_name, "r") as fh:
-    filedata = fh.read()
-    option_pattern = re.compile(r'\{::options(.*?)/\}')
-    options = []
-    for option in re.findall(option_pattern, filedata):
-        data = option.split(" ")
-        entry = {}
-        pattern = re.compile(r' (.*?)=\"(.*?)\"')
-        for k, v in re.findall(pattern, option):
-            entry[k] = v
-        options.append(entry)
-    
-    options_to_tests(draft_name, output_path, options)
+def parse_options_from_draft(draft_name):
+    with open(draft_name, "r") as fh:
+        filedata = fh.read()
+        option_pattern = re.compile(r'\{::options(.*?)/\}')
+        options = []
+        for option in re.findall(option_pattern, filedata):
+            data = option.split(" ")
+            entry = {}
+            pattern = re.compile(r' (.*?)=\"(.*?)\"')
+            for k, v in re.findall(pattern, option):
+                entry[k] = v
+            options.append(entry)
+        return options
 
+def parse_options(draft_name, output_path):
+    options = parse_options_from_draft(draft_name)
+    test_summary = create_test_summary_from_options(draft_name, options)
+    with open(os.path.join(output_path, draft_to_test_name(draft_name)), "w") as fh:
+        fh.write("export default\n")
+        fh.write(json.dumps(test_summary, indent=4))
+
+if __name__ == '__main__':
+    draft_name = sys.argv[1]
+    output_path = sys.argv[2]
+    parse_options(draft_name, output_path)
